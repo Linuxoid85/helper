a156472619e41fbfcc7d3d1b8afa1e5da2db89af
Linuxoid85 <71516578+Linuxoid85@users.noreply.github.com>
2020-11-03 01:05:58 +0300

    Helper - settings
    Настройки Helper. Переработал алгоритм чтения конфига, теперь всё проще и быстрее. На стадии добавления переключалки языков.

--- fl/settings.sh
+++ fl/settings.sh
@@ -1,9 +1,17 @@
+# ** #
+
+# ** #
 VERSION="0.06.2020"
-DIALOG=${DIALOG=dialog}
+tempfile=~/.helper.tmpfile
+update_file=~/.helper.distr.update
 DATE=`date`
-config_file=~/.helper.config_file	#Файл с настройками
+config_file=~/.helper.config_file
 show_welcome=welcome			#Стартовый экран
-ui=update
+ru=russian
+ua=ukrainian
+ui=strt
+tmpfile=~/.helper.cnf.tmpfile
+# ** #
 
 # Если файл есть -- выводим соответствующее сообщение.
 # Если файла нет -- создаём и пишем в него значения
@@ -17,6 +25,7 @@ else
 	echo "$ui" > $config_file
 fi
 
+# А это процедура чтения конфига.
 if grep $show_welcome $config_file; then
 	shw="ON"
 else
@@ -29,7 +38,51 @@ else
 	up="off"
 fi
 
-$DIALOG --backtitle "Helper $VERSION -- настройки программы. Дата: $DATE" --title " Настройки " --clear \
-	--checklist "Выберите нужный пункт [Space] и нажмите [Enter]:" 15 55 15 \
-		"$show_welcome" "Отображение меню приветствия" $shw \
-		"$ui" "Автообновление" $up 2> $config_file
+if grep $ru $config_file; then
+	shw1="ON"
+else
+	shw1="off"
+fi
+
+if grep $ua $config_file; then
+	shw2="ON"
+else
+	shw2="off"
+fi
+
+menu_inf="Выберите нужный пункт и нажмите [Enter]:"
+menu_1="Настройки Helper"
+# ** #
+# Старт Hlp-DialogUI
+start_st(){
+settings=$(dialog --backtitle "Helper $VERSION -- $menu_1. Дата: $DATE" --title "Настройки" \
+	--menu "$menu_inf" 16 58 15 \
+		"lng" "Язык программы" \
+		"appear" "Внешний вид программы" \
+		"ext" "Выход в главное меню")
+echo $settings > $tmpfile
+}
+
+start_st
+
+if grep 'lng' $tmpfile; then
+	settings_lng=$(dialog --backtitle "Helper $VERSION -- $menu_1" --title " $menu_1 " \
+		--radiolist "$menu_inf" 16 58 15 \
+			"$ru" "Русский язык" $shw1 \
+			"$ua" "Украинский" $shw2 2> $tempfile)
+	start_st
+fi
+
+if grep 'appear' $tmpfile; then
+	settings_appear=$(dialog --backtitle "Helper $VERSION -- $menu_1" --title "Настройки внешнего вида" \
+		--checklist "$menu_inf" 16 58 15 \
+			"$show_welcome" "Отображение меню приветствия" $shw)
+	start_st
+fi
+
+echo "Language = [$settings_lng]
+Appear = [$settings_appear]" > $config_file
+
+if grep 'ext' $tmpfile; then
+	helper
+fi
